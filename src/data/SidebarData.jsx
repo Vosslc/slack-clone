5bc5fd6f56b397d0f41cb33214c7e01e2a977438
Roundy <nroundy801@gmail.com>
2021-02-26 14:58:07 -0700

    first push to slack-clone

---
+++ src/data/SidebarData.jsx
@@ -0,0 +1,47 @@
+import MessageIcon from '@material-ui/icons/Message';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleIcon from '@material-ui/icons/People';
+import AppsIcon from '@material-ui/icons/Apps';
+import ScatterPlotIcon from '@material-ui/icons/ScatterPlot';
+
+
+
+export const sidebarItemsData = [
+  {
+    icon: <MessageIcon/>,
+    text: "Thread"
+  },
+  {
+    icon: <InboxIcon/>,
+    text: "All DM's"
+  },
+  {
+    icon: <DraftsIcon/>,
+    text: "Metions & Reaction"
+  },
+  {
+    icon: <BookmarkBorderIcon/>,
+    text: "Saved Items"
+  },
+  {
+    icon: <PeopleIcon/>,
+    text: "People & Groups"
+  },
+  {
+    icon: <AppsIcon/>,
+    text: "More"
+  },
+]
+
+export const sidebarChannelsData = [
+  {
+    icon: <ScatterPlotIcon/>,
+    text: "Channel 1"
+  },
+  {
+    icon: <ScatterPlotIcon/>,
+    text: "Channel 2"
+  },
+]
