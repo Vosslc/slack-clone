73f956d64229eced3f694d7005b2258d61601340
Roundy <nroundy801@gmail.com>
2021-02-26 22:14:27 -0700

    added the base for toggling between Dark mode and Light mode

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import reactDom from "react-dom";
+import styled, { ThemeProvider } from "styled-components";
 import Chat from "./components/Chat";
 import Login from "./components/Login";
-import styled from "styled-components";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { lighTheme, darkTheme, GlobalStyles } from "./themes/themes";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div className='App'>
-      <Router>
-        <Container>
-          <Header />
-          <Main>
-            <Sidebar />
-            <Switch>
-              <Route path='/room'>
-                <Chat />
-              </Route>
-              <Route path='/'>
-                <Login />
-              </Route>
-            </Switch>
-          </Main>
-        </Container>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lighTheme : darkTheme}>
+      <GlobalStyles />
+      <div className='App'>
+        <Router>
+          <Container>
+          <button onClick={()=>themeToggler()}>Change Theme</button>
+            <Header />
+            <Main>
+              <Sidebar />
+              <Switch>
+                <Route path='/room'>
+                  <Chat />
+                </Route>
+                <Route path='/'>
+                  <Login />
+                </Route>
+              </Switch>
+            </Main>
+          </Container>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
@@ -40,7 +52,7 @@ const Container = styled.div`
 `;
 
 const Main = styled.div`
-  background: white;
+  /* background: white; */
   display: grid;
   grid-template-columns: 260px auto;
 `;
